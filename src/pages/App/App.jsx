889cea6533bcfcc4e925c670d997560305c93e74
John McCants <johnmccants002@gmail.com>
2023-02-14 19:39:00 -0800

    Data pulled and received

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -9,9 +9,12 @@ import HeroHomePage from '../HeroHomePage/HeroHomePage';
 import NavBar from '../../components/NavBar/NavBar';
 import ProfilePage from '../ProfilePage/ProfilePage';
 import Footer from '../../components/Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import FoodShowPage from '../FoodShowPage/FoodShowPage'
 
 export default function App() {
   const [user, setUser] = useState(getUser());
+  const navigate = useNavigate()
 
   return (
     <main style={{backgroundColor: "#F3F3F3"}}>
@@ -21,9 +24,11 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}\
-              <Route path="/hero" element={<HeroHomePage user={user}/>}/>
-              <Route path="/hero/create" element={<HeroPostPage />} />
+              
+              <Route path="/hero/create" element={<HeroPostPage user={user}/>} />
               <Route path="/profile" element={<ProfilePage user={user}/>}/>
+              <Route path="/" element={<HeroHomePage user={user} navigate={navigate}/>}/>
+              <Route path="/post/:id" element={<FoodShowPage />} />
             </Routes>
             <Footer />
           </>
--- src/pages/HeroHomePage/HeroHomePage.jsx
+++ src/pages/HeroHomePage/HeroHomePage.jsx
@@ -2,7 +2,8 @@ import * as foodsAPI from '../../utilities/food-api';
 import { useEffect, useState } from 'react';
 import './HeroHomePage.css'
 
-export default function HeroHomePage({user}) {
+
+export default function HeroHomePage({user, navigate}) {
 
     const [posts, setPosts] = useState([])
 
@@ -30,7 +31,7 @@ export default function HeroHomePage({user}) {
 <div  className="md:flex-auto home_pg flex-wrap">
     {posts && posts.map((post, i) => {
         return (
-            <div key={i} className="mb-4 w-1/3 h-2/3">
+            <div key={i} className="mb-4 w-1/3 h-2/3" onClick={() => navigate(`/post/${post._id}`)}>
                 <div className="bck-clr max-w-sm rounded overflow-hidden shadow-lg text-red-800">
                         <div className='imgDiv'><img className="postImg" src={post.image ? post.image : post.photoUrl} alt="Food image"></img></div>
                     <div className="px-6 py-4">
@@ -45,7 +46,7 @@ export default function HeroHomePage({user}) {
                         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Location: {post.location}</span>
                     </div>
                     <div>
-                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
   Request
 </button>
 </div>
