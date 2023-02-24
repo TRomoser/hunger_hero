5271e1a7a7f2806133a2d09327d60045bf32c243
Taylor Romoser <TRomoser@gmail.com>
2023-02-23 19:03:41 -0800

    fixing sign up form

--- controllers/api/foods.js
+++ controllers/api/foods.js
@@ -8,37 +8,38 @@ module.exports = {
 }
 
 async function create(req, res) {
-    try {
-      // Add the food to the db
-      console.log("THIS IS REQ.USER", req.user)
-      console.log(req.body, "food object")
-      req.body.user = req.user._id
-      console.log("AFTER", req.body)
-      const food = await Food.create(req.body);
-      
-    
-      res.json(food);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  }
-
-  async function getFood(req, res) {
-    console.log("GET FOOD CALLED")
-    console.log("REQ.BODY", req.params)
-    
-
-    const food = await Food.findById(req.params.id)
-    console.log("FOOD", food)
-    res.json(food)
-  }
-
-  async function index(req, res){
-    console.log('index almost called')
-    const food = await Food.find({}).exec()
-    console.log(food, 'index fnc called')
+  try {
+    // Add the food to the db
+    console.log("THIS IS REQ.USER", req.user)
+    console.log(req.body, "food object")
+    req.body.user = req.user._id
+    console.log("AFTER", req.body)
+    const food = await Food.create(req.body);
     res.json(food);
+  } catch (err) {
+    res.status(400).json(err);
   }
+}
+
+async function getFood(req, res) {
+  console.log("GET FOOD CALLED")
+  console.log("REQ.BODY", req.params)
+  const food = await Food.findById(req.params.id)
+  console.log("FOOD", food)
+  res.json(food)
+}
+
+async function index(req, res){
+  console.log('index almost called')
+  const food = await Food.find({}).exec()
+  console.log(food, 'index fnc called')
+  res.json(food);
+}
+
+
+
+
+
 
   // async function deleteFood(req, res) {
   //   req.body.user = req.user._id;
