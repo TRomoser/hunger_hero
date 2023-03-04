c24c0e0c41bbf67f53c82ec6c2c2367cb3d3009a
achasek <chaseknieper@gmail.com>
2023-03-03 19:56:25 -0700

    hero landing page

--- src/pages/HeroRequestPage/HeroRequestPage.jsx
+++ src/pages/HeroRequestPage/HeroRequestPage.jsx
@@ -1,6 +1,6 @@
 export default function HeroRequestPage() {
   return (
-    <div>
+    <div className="body">
       <h1 style={{'font-size': '68px', 'text-align': 'center'}}>This will be the requests page</h1>
     </div>
   );
