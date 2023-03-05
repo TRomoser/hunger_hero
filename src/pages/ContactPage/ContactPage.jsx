550c371553ba953ee3682b3e5d16fad61e62586e
achasek <chaseknieper@gmail.com>
2023-03-05 16:02:21 -0700

    contact cards in ContactPage

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,28 +1,45 @@
+import './ContactPage.css'
+
 export default function ContactPage() {
     return (
         <div className="body">
             <div className="taylor">
-                <h2>Taylor Romoser</h2>
-                <a href="https://github.com/TRomoser" target="_blank"><h2>GitHub</h2></a>
-                <a href="https://www.linkedin.com/in/taylor-romoser/" target="_blank"><h2>LinkedIn</h2></a>
+                <h2 className='name'><strong>Taylor Romoser</strong></h2>
+                <h2>Software Engineer</h2>
+                <a href="https://github.com/TRomoser" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/2111/2111432.png" alt="GitHub"/></a>
+                <a href="https://www.linkedin.com/in/taylor-romoser/" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="LinkedIn"/></a>
                 <a
                     rel="noreferrer"
                     target="_blank"
                     class="cta-btn cta-btn--resume"
                     href="mailto:TaylorRomoser@gmail.com"
-                    >Email  
+                    ><img className="icon" src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email"/>
                 </a>
             </div>
             <div className="chase">
-                <h2>Chase Allman-Knieper</h2>
-                <a href="https://github.com/achasek" target="_blank"><h2>GitHub</h2></a>
-                <a href="https://www.linkedin.com/in/chase-ak/" target="_blank"><h2>LinkedIn</h2></a>
+                <h2 className='name'><strong>Chase Allman-Knieper</strong></h2>
+                <h2>Software Engineer</h2>
+                <a href="https://github.com/achasek" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/2111/2111432.png" alt="GitHub"/></a>
+                <a href="https://www.linkedin.com/in/chase-ak/" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="LinkedIn"/></a>
                 <a
                     rel="noreferrer"
                     target="_blank"
                     class="cta-btn cta-btn--resume"
                     href="mailto:chaseknieper@gmail.com"
-                    >Email  
+                    ><img className="icon" src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email"/>  
+                </a>
+            </div>
+            <div className="john">
+                <h2 className='name'><strong>John McCants</strong></h2>
+                <h2>Software Engineer</h2>
+                <a href="https://github.com/johnmccants002" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/2111/2111432.png" alt="GitHub"/></a>
+                <a href="https://www.linkedin.com/in/johnmccants" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="LinkedIn"/></a>
+                <a
+                    rel="noreferrer"
+                    target="_blank"
+                    class="cta-btn cta-btn--resume"
+                    href="mailto:johnmccants002@gmail.com"
+                    ><img className="icon" src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email"/> 
                 </a>
             </div>
         </div>
