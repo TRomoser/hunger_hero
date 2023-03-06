eb0c764e8f524de2c88d011dd652ee1a33953b1d
achasek <chaseknieper@gmail.com>
2023-03-05 19:29:21 -0700

    updated about page and footer

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,29 +2,44 @@ import './AboutPage.css';
 
 export default function AboutPage() {
     return (
-        <div className="body">
+        <div className="aboutBody">
             <div className='infoText'>
-                <h2>
-                    According to the Food and Agriculture Organization of the United Nations, approximately one-third of all food produced in the world is lost or wasted. This amounts to 1.3 billion tons of food that is never consumed, which could feed an estimated 3 billion people.
-                
-                </h2>
-                <br />
-                <h2>
-                    In North America alone, it is estimated that nearly 40% of all food produced is never consumed. This is despite the fact that there are millions of people in the region who suffer from food insecurity and do not have access to adequate nutrition.
-                </h2>
-                <br />
-                <h2>
-                    Food waste is not only a moral issue but also an environmental one. When food is wasted, all the resources that were used to produce it, including land, water, energy, and labor, are also wasted. Additionally, food waste is a significant source of greenhouse gas emissions, contributing to climate change.
-                </h2>
-                <br />
-                <h2>
-                    HungerHero is committed to reducing food waste and increasing access to nutritious food for those in need. By working with food recovery organizations, restaurants, and grocery stores, HungerHero helps redirect surplus food to people who are experiencing food insecurity. This not only reduces food waste but also helps alleviate hunger and improve nutrition.
-                </h2>
-                <br />
-                <h2><strong>
-                    Join us in the fight against food waste and hunger by supporting HungerHero today.
-                </strong>
-                </h2>
+                <div>
+                    <h2>
+                        According to the Food and Agriculture Organization of the United Nations, approximately one-third of all food produced in the world is lost or wasted. This amounts to 1.3 billion tons of food that is never consumed, which could feed an estimated 3 billion people.
+                    
+                    </h2>
+                    <br />
+                    <br />
+                </div>
+                <div>
+                    <h2>
+                        In North America alone, it is estimated that nearly 40% of all food produced is never consumed. This is despite the fact that there are millions of people in the region who suffer from food insecurity and do not have access to adequate nutrition.
+                    </h2>
+                    <br />
+                    <br />
+                </div>
+                <div>
+                    <h2>
+                        Food waste is not only a moral issue but also an environmental one. When food is wasted, all the resources that were used to produce it, including land, water, energy, and labor, are also wasted. Additionally, food waste is a significant source of greenhouse gas emissions, contributing to climate change.
+                    </h2>
+                    <br />
+                    <br />
+                </div>
+                <div>
+                    <h2>
+                        HungerHero is committed to reducing food waste and increasing access to nutritious food for those in need. By working with food recovery organizations, restaurants, and grocery stores, HungerHero helps redirect surplus food to people who are experiencing food insecurity. This not only reduces food waste but also helps alleviate hunger and improve nutrition.
+                    </h2>
+                    <br />
+                    <br />
+                    <br />
+                </div>
+                <div>
+                    <h2><strong>
+                        Join us in the fight against food waste and hunger by supporting HungerHero today.
+                    </strong>
+                    </h2>
+                </div>
             </div>
             <div className='triangle'>
 
@@ -66,8 +81,11 @@ export default function AboutPage() {
                     </li>
                 </ul>
             </div>
-            <div className='rectangle'>
+            <div className='bottomTri'>
                 
+            </div>
+            <div className='rectangle'>
+
             </div>
         </div>
     )
