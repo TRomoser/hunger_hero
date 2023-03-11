82878244c3d78a6773e8ae528dd7d55ffa7b93c0
achasek <chaseknieper@gmail.com>
2023-03-11 16:10:38 -0700

    updated styling on login and added greg to contacts

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -42,6 +42,19 @@ export default function ContactPage() {
                     ><img className="icon" src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email"/> 
                 </a>
             </div>
+            <div className="greg">
+                <h2 className='name'><strong>Greg Delgado</strong></h2>
+                <h2>UX Designer</h2>
+                <a href="http://www.gregdelgado.com/" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/281/281089.png" alt="Portfolio"/></a>
+                <a href="https://www.linkedin.com/in/greg-delgado-0a506a122" target="_blank"><img className="icon" src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="LinkedIn"/></a>
+                <a
+                    rel="noreferrer"
+                    target="_blank"
+                    class="cta-btn cta-btn--resume"
+                    href="mailto:GregDelgado94@gmail.com"
+                    ><img className="icon" src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email"/> 
+                </a>
+            </div>
         </div>
     )
 }
