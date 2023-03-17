66066223a8462822e89f01213544496f1e63ffcd
achasek <chaseknieper@gmail.com>
2023-03-16 17:26:13 -0700

    fixed landing page

--- src/pages/HeroLandingPage/HeroLandingPage.jsx
+++ src/pages/HeroLandingPage/HeroLandingPage.jsx
@@ -50,7 +50,10 @@ export default function HeroLandingPage() {
         <h2>Donate goods</h2>
       </div>
       <div className="facts">
-        <h1>facts</h1>
+        <img className="factsImg" src="./images/factsImg.png" />
+        <div className="factsText">
+          <p>Approximately one third of all food produced in the world is wasted every year, amounting to approximately 1.3 billion tons. This food waste contributes to resource depletion, greenhouse gas emissions, and economic losses, as well as exacerbating global hunger and poverty. Reducing food waste is an important part of addressing these challenges and creating a more sustainable food system.</p>
+        </div>
       </div>
     </div>
   );
